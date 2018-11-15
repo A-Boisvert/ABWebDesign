52d86f7add42974eec897c08def384cc5d8b3251
Andrew <andrew@boisverts.us>
2018-11-15 18:07:11 -0500

    Added "Click Us" img to exercise 12
    Uploaded wireframes and digital mockups for the Large Desktop size of Project 4

---
+++ js/project_04.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+	
+	$(window).resize(function() {
+		
+		
+		waitForFinalEvent(function(){
+			//alert('Resize...');
+			
+		}, 500, "some unique string");
+	});
+	
+	$(".hamburger").click(function(){
+		
+	});
+	
+	
+	$(".mobile, .tablet, .dekstop, .largeDesktop").click(function()
+	{
+		console.log('current Offset is ' + currentShift);
+	});
+	
+});
+
+var currentWindowSize = 0;
+
+
+function CheckBackStem ()
+{
+	
+}
+
+function CheckWindowSize()
+{
+	if ($(window).width() > 1024)
+		currentWindowSize = 4;
+	else if ($(window).width() <= 1024)
+		currentWindowSize = 3;
+	if ($(window).width() <= 768)
+		currentWindowSize = 2;
+	if ($(window).width() <= 482)
+		currentWindowSize = 1;
+	
+	//$('.debugBlock').text(' ' + currentWindowSize);
+	
+	AdjustWindowSensitiveVariables();
+}
+
+function AdjustWindowSensitiveVariables()
+{
+	switch (currentWindowSize){
+		
+		case 1:
+			
+		break;
+		
+		case 2:
+			
+		break;
+		
+		case 3:
+			
+		break;
+		
+		case 4:
+			
+		break;
+	}
+}
+
+// gotten from https://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
